b8e513930f4de9e829785806f82c2d2af60a96c4
kenjay24 <201940243@psu.palawan.edu.ph>
2025-04-08 16:55:46 +0800

    organizationlist

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -7,4 +7,10 @@ class HomePageView(ListView):
     context_object_name = 'home'
     template_name = "home.html"
 
+class OrganizationList(ListView):
+    model = Organization
+    template_name = 'organization_list.html'  
+    context_object_name = 'organization'
+    paginate_by = 5
+
 # Create your views here.
