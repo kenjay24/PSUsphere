cf5c155d17c2d0ac0d697767e967052c31555e7b
kenjay24 <201940243@psu.palawan.edu.ph>
2025-04-10 17:22:13 +0800

    search feature working

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -5,6 +5,10 @@ from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
 
+from typing import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q
+
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
@@ -15,6 +19,13 @@ class OrganizationList(ListView):
     template_name = 'organization_list.html'  
     context_object_name = 'organization'
     paginate_by = 5
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        query = self.request.GET.get("q")
+        if query:
+            qs = qs.filter(Q(name__icontains=query) | Q(college__college_name__icontains=query) | Q(description__icontains=query))
+        return qs
 
 
 class OrganizationCreateView(CreateView):
