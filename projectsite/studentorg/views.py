47c64f0f1864ecdcfd0f2e23976206a933d8f3c9
kenjay24 <201940243@psu.palawan.edu.ph>
2025-04-10 14:15:14 +0800

    fix org error

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Organization
@@ -12,5 +15,12 @@ class OrganizationList(ListView):
     template_name = 'organization_list.html'  
     context_object_name = 'organization'
     paginate_by = 5
+
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name= 'organization_add.html'
+    success_url = reverse_lazy('organization-list')
 
 # Create your views here.
