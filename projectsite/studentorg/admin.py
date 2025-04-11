3bc6e9828fea64f5f44f52f7a28198ff5b0d95ef
CharlesJazon <202080041@psu.palawan.edu.ph>
2025-04-11 09:11:31 +0800

    working login and logout

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -9,13 +9,15 @@ admin.site.register(Organization)
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    list_display = ("student_id", "lastname",
+                    "firstname", "middlename", "program")
     search_fields = ("lastname", "firstname")
 
 @admin.register(OrgMember)
-class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined")
-    search_fields = ("student__lastname", "student__firstname")
+class OrgMemberInline(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization","date_joined")
+    search_fields = ("student_lastname", "student_firstname")
+
 
     def get_member_program(self, obj):
         try:
